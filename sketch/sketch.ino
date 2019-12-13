3b980ddc65a8f39ba629fae179d3065d38d8aaa6
Matveyushka <waytokey@mail.ru>
2019-12-13 21:19:35 +0300

    add parallel listening

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,15 +6,23 @@
 #define LED D0
 #define BTN D6
 #define SND A0
-#define RECORDING_STEPS_AMOUNT 10
-#define SENDED_STEPS_AMOUNT 600
+
 #define THRESHOLD_VALUE 200
-#define OUTPUT_SPEED 115200
+#define OUTPUT_SPEED_IN_BODS 115200
 
-#define BACKEND_ADRESS "http://192.168.0.100:1234/history"
+#define SENDED_STEPS_AMOUNT 60
 
-#define _SSID "437"
-#define PASSWORD "orda_sasat"
+#define TOUCH_SENSOR_READING_STEP_MS 100
+#define TOUCH_SENSOR_ACTIVATION_TIME_MS 1000
+
+#define SOUND_LOOP_STEP 10
+
+#define BACKEND_ADRESS "http://192.168.43.116:1234/history"
+
+#define _SSID "the_best_wifi"
+#define PASSWORD "zaslonka"
+
+bool isOn = false;
 
 void connectToWifi()
 {
@@ -41,26 +49,12 @@ void connectToWifi()
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(BTN, INPUT);
   
   delay(1000);
-  Serial.begin(OUTPUT_SPEED);
+  Serial.begin(OUTPUT_SPEED_IN_BODS);
   
   connectToWifi();
-}
-
-int recordSound(int readingStepsAmount)
-{
-  int value = 0;
-  for (int i = 0; i != readingStepsAmount; i++)
-  {
-    int analogInput = analogRead(SND);
-    if (analogInput > value)
-    {
-      value = analogInput;
-    }
-    delay(10);
-  }
-  return value;
 }
 
 int indicateThreshold(int value, int thresholdValue)
@@ -83,27 +77,87 @@ void sendValues(int values[SENDED_STEPS_AMOUNT])
   http.addHeader("Content-Type", "text-plain");
   int httpCode = http.POST(postData);   
   String payload = http.getString();    
- 
   Serial.println(httpCode);  
   Serial.println(payload);    
  
   http.end(); 
 }
 
-int onoff = 1;
-int btnstate = 0;
+void switchLedOn() { digitalWrite(LED, HIGH); }
+void switchLedOff() { digitalWrite(LED, LOW); }
 
-void loop() {
-  Serial.println("working");
-  int values[SENDED_STEPS_AMOUNT];
-  
-  for (int i = 0; i != SENDED_STEPS_AMOUNT; i++)
+
+/*        Touch sensor loop        */
+int touchSensorLastRead = -TOUCH_SENSOR_READING_STEP_MS;
+int touchSensorState = 0;
+void activationTouchSensorLoop()
+{
+  if (millis() - touchSensorLastRead >= TOUCH_SENSOR_READING_STEP_MS)
   {
-    int value = recordSound(RECORDING_STEPS_AMOUNT);
-    Serial.println(String(i) + " :: " + String(value));
-    indicateThreshold(value, THRESHOLD_VALUE);
-    values[i] = value;
+    touchSensorLastRead = millis();
+    touchSensorState = digitalRead(BTN) == 1 ? touchSensorState + 1 : 0;
+    if (touchSensorState == TOUCH_SENSOR_ACTIVATION_TIME_MS / TOUCH_SENSOR_READING_STEP_MS)
+    {
+      isOn = !isOn;
+      if (isOn) switchLedOn();
+      else 
+      {
+        switchLedOff();
+        resetRecorded();
+      }
+    }
   }
-  
-  sendValues(values);
+}
+
+/*         Sound recording loop       */
+int soundLoopLastRead = -SOUND_LOOP_STEP;
+int values[SENDED_STEPS_AMOUNT];
+int valuesPointer = 0;
+
+int recordSteps = 10;
+int recordedStep = 0;
+int recordedValue = 0;
+void record()
+{
+  int soundInput = analogRead(SND);
+  if (soundInput > recordedValue) recordedValue = soundInput;
+  recordedStep += 1;
+  if (recordedStep == recordSteps)
+  {
+    Serial.println(String(valuesPointer) + " :: " + String(recordedValue));
+    values[valuesPointer] = recordedValue;
+    recordedStep = 0;
+    recordedValue = 0;
+    valuesPointer += 1;
+    if (valuesPointer == SENDED_STEPS_AMOUNT)
+    {
+      valuesPointer = 0;
+      sendValues(values);
+    }
+  }
+}
+
+void resetRecorded()
+{
+  recordedValue = 0;
+  recordedStep = 0;
+  valuesPointer = 0;
+}
+
+void soundLoop()
+{
+  if (millis() - soundLoopLastRead >= SOUND_LOOP_STEP)
+  {
+    soundLoopLastRead = millis();
+    if (isOn)
+    {
+      record();
+    }
+  }
+}
+
+/*      Main loop      */
+void loop() {
+  activationTouchSensorLoop();
+  soundLoop();
 }
